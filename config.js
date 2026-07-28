0bbaf125bff12ede9ea9292891c891b3cd79d473
junhak2520-web <junhak2520@gmail.com>
2026-07-28 14:17:09 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Supabase 연결 정보
+window.SUPABASE_URL = "https://anwazpyepeqkbcxhnjsr.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFud2F6cHllcGVxa2JjeGhuanNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxODkzMDUsImV4cCI6MjEwMDc2NTMwNX0.xKDnOSClTqz1ciiYek3XI7r2lDrPFElAUKvfaprKAa8";
