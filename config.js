8677566da577033596d116f787d38372cafa2872
junhak2520-web <junhak2520@gmail.com>
2026-08-14 13:33:43 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// Supabase 연결 정보
+// Supabase 연결 정보 (배당앱과 동일 프로젝트, 테이블만 tj_trades로 분리)
 window.SUPABASE_URL = "https://anwazpyepeqkbcxhnjsr.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFud2F6cHllcGVxa2JjeGhuanNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxODkzMDUsImV4cCI6MjEwMDc2NTMwNX0.xKDnOSClTqz1ciiYek3XI7r2lDrPFElAUKvfaprKAa8";
